c4e428b217c0d6827a9e992e160ee2231b09cd04
wuyun1 <842269153@qq.com>
2016-06-03 20:05:33 +0800

    fd

--- src/com/CCL/Dao/CustomerDao.java
+++ src/com/CCL/Dao/CustomerDao.java
@@ -16,8 +16,25 @@ public interface CustomerDao  {
 	
 	List<Customer> queryByType(CustomerType customerType);
 	
+	
+	/**
+	 * 分页模糊查询 
+	 * @param property	需要进行筛选的属性字段(对象的属性名)
+	 * @param value	 模糊匹配的值
+	 * @param pageSize   页面大小
+	 * @param pageNumber  第几页
+	 * @return
+	 */
 	List<Customer> queryByUseLikeAndPage(String  property,String value,int pageSize,int pageNumber);
 	
+	
+	/**
+	 * 分页模糊查询 
+	 * @param entrys	属性和值的键值对
+	 * @param pageSize   页面大小
+	 * @param pageNumber  第几页
+	 * @return
+	 */
 	List<Customer> queryByUseLikeAndPage(Map<String,String> entrys,int pageSize,int pageNumber);
 
 	List<Customer> queryByState(CustomerState customerState);
